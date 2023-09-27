3705c95b13d9d06d959444ee85191c8a7d9ae507
abhinaymuthineni <muthineniabhinay@gmail.com>
2023-09-27 18:17:19 +0530

    entire backend code

--- server/src/routes/timeManagementRoutes.js
+++ server/src/routes/timeManagementRoutes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const timeManagementController = require('../controllers/userController')
 
 router.post('/checkIn', timeManagementController.checkIn);
+router.post('/breakStart/:user_id', timeManagementController.breakStartAndEnd)
+router.put('/checkOut/:user_id', timeManagementController.checkOut)
 
 module.exports = router;
